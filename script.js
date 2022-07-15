debe9bc9470759da1f3b4f5fc886daba77eead92
wj-tan <1901864@sit.singaporetech.edu.sg>
2022-07-15 14:15:11 +0800

    Drew circle

--- script.js
+++ script.js
@@ -8,16 +8,50 @@ fetch(apiUrl)
 
         // Store Json data in environmentData variable
         environmentData = data.items[0].readings
-        console.log(environmentData)
+        locationData = data.region_metadata[0].label_location
+        // console.log(environmentData)
+        console.log(locationData)
 
-        Object.keys(environmentData).map(key => {
-            console.log(environmentData[key]);
+        // var longitute1 = data.region_metadata[0].label_location.
+        var latitude1 = data.region_metadata[0].label_location.latitude
+        var longitude1 = data.region_metadata[0].label_location.longitude
 
-        })
+        // Omitted 
+        // var latitude2 = data.region_metadata[1].label_location.latitude
+        // var longitude2 = data.region_metadata[1].label_location.longitude
 
-        Object.values(environmentData).map(value => {
-            console.log(value);
-        })
+        var latitude2 = data.region_metadata[2].label_location.latitude
+        var longitude2 = data.region_metadata[2].label_location.longitude
+
+        var latitude3 = data.region_metadata[3].label_location.latitude
+        var longitude3 = data.region_metadata[3].label_location.longitude
+
+        var latitude4 = data.region_metadata[4].label_location.latitude
+        var longitude4 = data.region_metadata[4].label_location.longitude
+
+        var latitude5 = data.region_metadata[5].label_location.latitude
+        var longitude5 = data.region_metadata[5].label_location.longitude
+
+        console.log(latitude2)
+        console.log(longitude2)
+
+        console.log(latitude3)
+        console.log(longitude3)
+
+        console.log(latitude4)
+        console.log(longitude4)
+
+        console.log(latitude5)
+        console.log(longitude5)
+
+        // Object.keys(environmentData).map(key => {
+        //     console.log(environmentData[key]);
+
+        // })
+
+        // Object.values(environmentData).map(value => {
+        //     console.log(value);
+        // })
 
         let tiles = new L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
             detectRetina: true,
@@ -35,6 +69,43 @@ fetch(apiUrl)
             maxBounds: L.latLngBounds(L.latLng(1.1, 103.5), L.latLng(1.5, 104.3))
         })
             .addLayer(tiles);
+
+
+        // Drawing the circle based on the latitude and longitude    
+        var circle1 = L.circle([latitude1, longitude1], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map);
+
+        var circle2 = L.circle([latitude2, longitude2], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map);
+
+        var circle3 = L.circle([latitude3, longitude3], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map);
+
+        var circle4 = L.circle([latitude4, longitude4], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map);
+
+        var circle5 = L.circle([latitude5, longitude5], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 500
+        }).addTo(map);
 
 
     });
